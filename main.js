d2f5bf14808ea80117ee50a52089699c62e89780
Sung Paik <sung.paik@codeimmersives.com>
2019-12-03 10:16:25 -0500

    completed exercise

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct = false) {
+  return {
+    species,
+    period,
+    carnivore,
+    extinct,
+  }
 }
 
-const makeSingular = function() {
+const makeSingular = function(dinosaur) {
+  const {species, period, carnivore, extinct} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, extinct);
+
+  if (species.endsWith('us')) {
+    newDino.species = species.slice(0, -2);
+  }
+
+  return newDino;
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(dinosaur) {
+  const {species, period, carnivore, extinct} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, extinct);
+  
+  if (species.length > 10) {
+    newDino.species = species.slice(0, 7) + '...';
+  }
+  
+  return newDino;
 }
 
-const makeExtinct = function() {
+const makeExtinct = function(dinosaur) {
+  const {species, period, carnivore} = dinosaur;
+  const newDino = makeDino(species, period, carnivore, true);
 
+  return newDino;
 }
 
 module.exports = {
